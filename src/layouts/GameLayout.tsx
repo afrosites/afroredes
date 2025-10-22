c5bee0f8fdf828220e88fe7551240adad41c182f
[dyad] <contato.afrosites@gmail.com>
2025-10-22 13:26:52 -0300

    [dyad] Tornando o menu lateral fixo no layout do jogo - wrote 1 file(s)

--- src/layouts/GameLayout.tsx
+++ src/layouts/GameLayout.tsx
@@ -3,6 +3,7 @@ import { Navbar } from '@/components/Navbar';
 import { Sidebar } from '@/components/Sidebar';
 import { Footer } from '@/components/Footer';
 import { useIsMobile } from '@/hooks/use-mobile';
+import { cn } from '@/lib/utils'; // Importar cn para combinar classes
 
 interface GameLayoutProps {
   children: React.ReactNode;
@@ -15,8 +16,12 @@ const GameLayout: React.FC<GameLayoutProps> = ({ children }) => {
     <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
       <Navbar />
       <div className="flex flex-1">
-        {!isMobile && <Sidebar />} {/* Desktop sidebar */}
-        <main className="flex-1 p-4 md:p-8">
+        {!isMobile && (
+          <div className="fixed top-16 left-0 h-[calc(100vh-4rem)] w-64 flex-shrink-0 z-30"> {/* Sidebar fixo */}
+            <Sidebar />
+          </div>
+        )}
+        <main className={cn("flex-1 p-4 md:p-8", !isMobile && "md:ml-64")}> {/* Conteúdo principal com margem para o sidebar */}
           {children}
         </main>
       </div>
